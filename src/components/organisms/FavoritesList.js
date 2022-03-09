f389d7b27efe67d1b227fe582f9928c984af183b
Victor-Boyer <victorboyer33@gmail.com>
2022-03-09 02:43:30 +0100

    [edit] fav share

--- src/components/organisms/FavoritesList.js
+++ src/components/organisms/FavoritesList.js
@@ -1,6 +1,9 @@
 import { useState, useContext } from "react";
 import { FavContext } from "../../context/fav";
-import { TypcnHeartFullOutline } from "../../helpers/fav.icons";
+import {
+  TypcnArrowForward,
+  TypcnHeartFullOutline,
+} from "../../helpers/fav.icons";
 import { removeFromFavorite } from "../../helpers/fav.setter";
 import { DefaultBtn } from "../atoms/Button";
 import { DefaultCard } from "../atoms/Card";
@@ -8,6 +11,7 @@ import { DefaultCard } from "../atoms/Card";
 export const FavoritesList = ({ setter }) => {
   const { favorites, setFavorites } = useContext(FavContext);
   const [loading, setLoading] = useState(false);
+  const [copy, setCopy] = useState(false);
   return (
     <div className="w-full h-screen">
       <div className="w-full h-full flex flex-col items-center">
@@ -30,55 +34,42 @@ export const FavoritesList = ({ setter }) => {
                       loading ? "hidden" : "block"
                     }`}
                   />
-                  <label className="flex items-center mt-2 gap-2">
-                    <TypcnHeartFullOutline
-                      className="cursor-pointer"
+                  <div className="flex items-center justify-between mt-4 gap-2">
+                    <label className="flex items-center gap-2">
+                      <TypcnHeartFullOutline
+                        className="cursor-pointer"
+                        onClick={() => {
+                          removeFromFavorite(
+                            (cat = { id: cat }),
+                            favorites,
+                            setFavorites
+                          );
+                        }}
+                      />
+                      Love it !
+                    </label>
+                    <label
+                      className="flex items-center gap-2 cursor-pointer"
                       onClick={() => {
-                        removeFromFavorite(
-                          (cat = { id: cat }),
-                          favorites,
-                          setFavorites
+                        navigator.clipboard.writeText(
+                          `https://watchcats.pages.dev/cat/${cat}`
                         );
+                        setCopy(true);
+                        setTimeout(() => setCopy(false), 4000);
                       }}
-                    />
-                    Love it !
-                  </label>
+                    >
+                      share the cat
+                      <TypcnArrowForward className="hvr-grow" />
+                      <span className="bg-green text-grey font-bold px-2 rounded">
+                        {copy && "Copied!"}
+                      </span>
+                    </label>
+                  </div>
                 </DefaultCard>
               </li>
             ))}
         </ul>
       </div>
     </div>
-    /*     <div className="w-full h-screen">
-      <div className="w-full h-full">
-        <div className="flex items-center justify-around py-5">
-          <h1 className="font-bold text-lg">My Favorites</h1>
-          <DefaultBtn text="Back to cats" onClick={() => setter(false)} />
-        </div>
-        <ul className="flex flex-col items-center overflow-scroll gap-8">
-          {favorites &&
-            favorites.map((cat) => (
-              <li key={cat.id}>
-                <DefaultCard className="flex flex-col justify-around">
-                  <img
-                    className="w-full"
-                    src={`${process.env.REACT_APP_URL}//cat/${cat}?type=sq`}
-                  />
-                  <TypcnHeartFullOutline
-                    className="cursor-pointer mt-2"
-                    onClick={() => {
-                      removeFromFavorite(
-                        (cat = { id: cat }),
-                        favorites,
-                        setFavorites
-                      );
-                    }}
-                  />
-                </DefaultCard>
-              </li>
-            ))}
-        </ul>
-      </div>
-    </div> */
   );
 };
